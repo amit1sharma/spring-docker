ddb437411982ac77ca48c0943bebeb102dec4a18
amit1sharma <funkiamit@gmail.com>
2021-06-14 16:33:17 +0400

    step for configuraing docker into linux

---
+++ src/main/java/com/example/springdocker/controller/HomePageController.java
@@ -0,0 +1,17 @@
+package com.example.springdocker.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+public class HomePageController {
+
+    /*@GetMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
+    public String getHomePage(Principal principal){
+//        if(principal.)
+        return "<body></body>";
+    }*/
+}
